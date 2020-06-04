84cd7297709bab963f5bc646e353f1b5ede84cce
Sara Kane <kane.sarak@gmail.com>
2020-06-04 10:00:10 -0700

    Add prompt for user to enter in and and functionality to show voting information based on age
    Co-authored-by: JohnNils Olson <johnnils@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -2,9 +2,9 @@ $(document).ready(function(){
   const age = parseInt(prompt("How old are you?"));
 
   if(age >= 18) {
-
+    $("#voting-information").show();
   }
   else {
-    
+    $("#minor").show();
   }
 });
